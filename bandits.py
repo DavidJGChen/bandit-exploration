266c600ecc161b994bf9d70564f9b881b34d8c72
DavidJGChen <33329617+DavidJGChen@users.noreply.github.com>
2025-06-17 15:08:32 -0700

    add some typing to bandits.py

--- bandits.py
+++ bandits.py
@@ -1,24 +1,64 @@
+from numpy import float64, dtype
+from numpy.typing import NDArray
 import numpy as np
+from abc import ABC, abstractmethod
+from typing import Generic, TypeVar, Any
 
-# TODO: add types
 
-
-class BaseArm:
-    def __init__(self, mean):
+class BaseArm[OutcomeType](ABC):
+    def __init__(self, mean: float) -> None:
         self.mean = mean
 
-    def sample(self):
+    @abstractmethod
+    def sample(self) -> OutcomeType:
         raise NotImplementedError
 
 
-class BaseBanditEnv:
+class BernoulliArm(BaseArm[float]):
+    def __init__(self, theta):
+        self.theta = theta
+        super().__init__(theta)
+
+    def sample(self) -> float:
+        return 1.0 if np.random.rand() < self.theta else 0.0
+
+
+class GaussianArm(BaseArm[float]):
+    def __init__(self, mu, eta):
+        self.mu = mu
+        self.eta = eta
+        super().__init__(mu)
+
+    def sample(self) -> float:
+        return np.random.normal(self.mu, self.eta)
+
+
+class PoissonArm(BaseArm[int]):
+    def __init__(self, rate):
+        self.rate = rate
+        super().__init__(rate)
+
+    def sample(self) -> int:
+        return np.random.poisson(self.rate)
+
+
+class LinearArm(BaseArm[float]):
+    def __init__(self, feature, theta):
+        self.feature = feature
+        super().__init__(feature @ theta)
+
+    def sample(self) -> float:
+        return self.mean + np.random.standard_normal()
+
+
+class BaseBanditEnv[A: BaseArm]:
     def __init__(self, K):
         self.K = K
-        self.arms = self.initialize_arms()
+        self.arms: list[A] = self.initialize_arms()
         self.optimal_action = np.argmax([arm.mean for arm in self.arms])
         self.optimal_mean = self.arms[self.optimal_action].mean
 
-    def initialize_arms(self):
+    def initialize_arms(self) -> list[A]:
         raise NotImplementedError
 
     def sample(self, action):
@@ -29,73 +69,42 @@ class BaseBanditEnv:
             raise
 
 
-class BernoulliBanditEnv(BaseBanditEnv):
-
-    class BernoulliArm(BaseArm):
-        def __init__(self, theta):
-            self.theta = theta
-            super().__init__(theta)
-
-        def sample(self):
-            return 1.0 if np.random.rand() < self.theta else 0.0
+class BernoulliBanditEnv(BaseBanditEnv[BernoulliArm]):
 
     def initialize_arms(self):
-        thetas = np.random.uniform(0, 1, self.K)
-        return [self.BernoulliArm(theta) for theta in thetas]
+        thetas: NDArray[float64] = np.random.uniform(0, 1, size=(self.K,))
+        return [BernoulliArm(theta) for theta in thetas]
 
 
-class GaussianBanditEnv(BaseBanditEnv):
-
-    class GaussianArm(BaseArm):
-        def __init__(self, mu, eta):
-            self.mu = mu
-            self.eta = eta
-            super().__init__(mu)
-
-        def sample(self):
-            return np.random.normal(self.mu, self.eta)
+class GaussianBanditEnv(BaseBanditEnv[GaussianArm]):
 
     def initialize_arms(self):
-        mus = np.random.normal(0, 1, self.K)
-        etas = np.ones(self.K)
-        return [self.GaussianArm(mu, eta) for mu, eta in zip(mus, etas)]
+        mus: NDArray[float64] = np.random.normal(0, 1, size=(self.K,))
+        etas: NDArray[float64] = np.ones(self.K)
+        return [GaussianArm(mu, eta) for mu, eta in zip(mus, etas)]
 
 
-class PoissonBanditEnv(BaseBanditEnv):
-
-    class PoissonArm(BaseArm):
-        def __init__(self, rate):
-            self.rate = rate
-            super().__init__(rate)
-
-        def sample(self):
-            return np.random.poisson(self.rate)
+class PoissonBanditEnv(BaseBanditEnv[PoissonArm]):
 
     def initialize_arms(self):
-        rates = np.random.exponential(1.0, size=self.K)
-        return [self.PoissonArm(rate) for rate in rates]
+        rates: NDArray[float64] = np.random.exponential(1.0, size=(self.K,))
+        return [PoissonArm(rate) for rate in rates]
 
 
-class LinearBanditEnv(BaseBanditEnv):
+class LinearBanditEnv(BaseBanditEnv[LinearArm]):
     def __init__(self, K, d):
         self.d = d
+        self.theta: NDArray[float64]
+        self.phi: NDArray[float64]
         super().__init__(K)
 
-    class LinearArm(BaseArm):
-        def __init__(self, feature, theta):
-            self.feature = feature
-            super().__init__(feature @ theta)
-
-        def sample(self):
-            return self.mean + np.random.standard_normal()
-
     def initialize_arms(self):
-        mean_vec = np.zeros(self.d)
-        covariance = 10 * np.eye(self.d)
+        mean_vec: NDArray[float64] = np.zeros(self.d)
+        covariance: NDArray[float64] = 10 * np.eye(self.d)
         feature_radius = 1 / np.sqrt(5)
         self.theta = np.random.multivariate_normal(mean_vec, covariance)
-        features = np.random.uniform(
+        features: NDArray[float64] = np.random.uniform(
             -feature_radius, feature_radius, size=(self.K, self.d)
         )
         self.phi = features
-        return [self.LinearArm(feature, self.theta) for feature in features]
+        return [LinearArm(feature, self.theta) for feature in features]
